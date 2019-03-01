cc32ab4be1c65169d1a82e8e954059f0f517e44f
Jacek91 <jaczaw91@gmail.com>
2019-03-01 20:42:37 +0100

    add Navigation component and styles to header

---
+++ src/js/Components/Navigation.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export class Navigation extends React.Component {
+  render() {
+    return (
+      <div className="header_container">
+        <header className="header">
+          <h1 className="header_title">Search Country application</h1>
+          <h2 className="header_subtitle">
+            Check the most important information about any country on the world
+          </h2>
+        </header>
+      </div>
+    );
+  }
+}
--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import '../scss/main.scss'
+import "../scss/main.scss";
 import { Countries } from "../js/Components/Countries.jsx";
+import { Navigation } from "../js/Components/Navigation.jsx";
 
-document.addEventListener("DOMContentLoaded", function () {
-    class App extends React.Component {
-        render() {
-            return (
-                <div>
-                    <Countries />
-                </div>
-            );
-        }
+document.addEventListener("DOMContentLoaded", function() {
+  class App extends React.Component {
+    render() {
+      return (
+        <div>
+          <Navigation />
+          <Countries />
+        </div>
+      );
     }
+  }
 
-    ReactDOM.render(<App />, document.getElementById("app"));
+  ReactDOM.render(<App />, document.getElementById("app"));
 });
-
