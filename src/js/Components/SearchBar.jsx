e85059a017b0a032ab4e4b578dfdcfc1fa2c16e2
Jacek91 <jaczaw91@gmail.com>
2019-03-02 19:56:21 +0100

    add changes and styled SearchBar component

--- src/js/Components/SearchBar.jsx
+++ src/js/Components/SearchBar.jsx
@@ -3,15 +3,29 @@ import React from "react";
 export class SearchBar extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      searchWord: ""
+    };
   }
 
+  handlSearchOnChange = event => {
+    let word = event.target.value;
+    this.setState({
+      searchWord: word
+    });
+
+    // word.length >= 3 ? this.filterCountriesData(word) : null;
+  };
+
   render() {
+    const { searchWord } = this.state;
     return (
       <section className="searchbar">
         <input
           type="text"
           className="searchbar_input"
+          value={searchWord}
+          onChange={this.handlSearchOnChange}
           tabIndex="0"
           placeholder="Type Country name in english"
         />
